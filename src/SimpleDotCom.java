024843aed4c51735d800c80c3661df7853726a65
Решетников Никита <reshetnikov@support-252.ALTEC>
2022-09-01 10:14:00 +0300

    new commit

---
+++ src/SimpleDotCom.java
@@ -0,0 +1,23 @@
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+    public void setLocationCells (int[] locs) {
+        locationCells = locs;
+    }
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Past";
+        for (int cell : locationCells){
+            if (guess == cell) {
+                result = "Got";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "Sunk";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
